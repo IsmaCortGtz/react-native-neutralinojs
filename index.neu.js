d7c5eabd796b9488f42bdd6f479079ea6d1f8260
IsmaCortGtz <ismacortgtz@gmail.com>
2025-11-09 02:04:22 -0600

    Move app run to template

--- index.neu.js
+++ index.neu.js
@@ -1,1 +1,8 @@
-import '..'
+import '..';
+import { AppRegistry } from 'react-native';
+import { name as appName } from '../app.json';
+
+AppRegistry.runApplication(appName, {
+  initialProps: {},
+  rootTag: document.getElementById('root'),
+});
